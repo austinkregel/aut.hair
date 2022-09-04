48d3f9cd62c841ebdf9c4771a9c6f3aff24ebaa3
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-09-04 02:28:11 +0000

    add synology blade

---
+++ resources/views/synology.blade.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+    <script>
+       var loc = window.location;
+        loc.replace(loc.pathname.loc.hash.replace('#', '?'));
+    </script>
+    </head>
+ <body>
+    <h1> Greetings, you'll  be redirected in a moment!</h1>
+ </body>
+ </html>
