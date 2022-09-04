7d28ca58c412a76ba1717d4696bdb4222828a153
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-09-04 01:25:52 +0000

    Add synology routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,15 @@ use Spatie\QueryBuilder\QueryBuilder;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/webman/sso/synoSSO-1.0.0.js', function () {
+    return response(file_get_contents(resource_path('synoSSO-1.0.0.js')), 200, [
+        'content-type' => 'application/javascript'
+    ]);
+});
+
+Route::get('/webman/sso/SSOOauth.cgi', function () {
+    return dd(request()->all());
+});
 
 Route::middleware([
     'auth:sanctum',
@@ -32,18 +41,18 @@ Route::middleware([
     return $request->user()->socials;
 });
 Route::get('/login/{provider}', function ($provider) {
-    abort_if(!in_array($provider, ['google', 'github']), 404, 'no provid');
+    abort_if(!in_array($provider, ['google', 'github', 'synology']), 404);
 
     return Socialite::driver($provider)->redirect();
 });
 
 Route::middleware('web')->get('/callback/{provider}', function ($provider) {
-    abort_if(!in_array($provider, ['google', 'github']), 404);
+    abort_if(!in_array($provider, ['google', 'github', 'synology']), 404);
 
     try {
         $user = Socialite::driver($provider)->user();
     } catch (\Exception $e) {
-        return redirect('/login');
+        return redirect('/login?message='.urlencode($e->getMessage()));
     }
 
     $social = Social::with('user')->firstWhere([
