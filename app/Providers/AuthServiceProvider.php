e0f4da507195881519b3318203f1aec249957554
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-09-04 04:19:31 +0000

    Remove deprecated auth system

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Idaas\Passport\Passport;
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
