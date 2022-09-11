6cf6f8ee123ce9678ef876e1330d7ca9e2d9afb8
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-09-11 13:53:16 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('userinfo', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('userinfo', function (Request $request) {    
+    $user = [];
+    
+    if (request->user()->tokenCan('openid') {
+        $user['id'] = auth()->id();
+        $user['updated_at'] => auth()->user()->updated_at;
+        $user['created_at'] => auth()->user()->created_at;
+    }
+    if (request->user()->tokenCan('profile')) {
+        $user['photo_url'] = auth()->user()->profile_photo_url;
+        $user['name'] = auth()->user()->name;
+    }
+        
+    if (request->user()->tokenCan('email')) {
+        $user['email'] = auth()->user()->email;
+        $user['email_verified_at'] = auth()->user()->eamil_verified_at;
+    }
+    
+    return response()->json($user);
 })->name('oidc.userinfo');
 
 Route::get('jwks', function () {
