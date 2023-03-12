772844de855c36c92fd768618bee0da9491363e5
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2023-03-12 14:13:35 -0400

    Update authorize.blade.php

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="dark">
+<html lang="en" class="dark h-full">
 <head class="h-full">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
