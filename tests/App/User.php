5d7446e66dff5600a2b170d45b4122cae51d8434
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2024-07-20 11:03:06 -0400

    Fix tests

--- tests/App/User.php
+++ tests/App/User.php
@@ -16,28 +16,19 @@ class User extends Authenticatable
      *
      * @var string[]
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * The attributes that should be cast.
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = ['email_verified_at' => 'datetime'];
 }
